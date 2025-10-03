45365697b85e52f09bb5850b60288811feff9a92
rizarayhan <rizarayhanw@gmail.com>
2025-10-03 17:23:08 +0700

    bug fix

--- src/components/SearchSection.jsx
+++ src/components/SearchSection.jsx
@@ -7,7 +7,7 @@ const SearchSection = ({ getWeatherDetails, searchInputRef }) => {
   const handleCitySearch = (e) => {
     e.preventDefault();
     const searchInput = e.target.querySelector(".search-input");
-    const API_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${searchInput.value}&days=2`;
+    const API_URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${searchInput.value}&days=2`;
     getWeatherDetails(API_URL); //Fetches weather details for the entered city
   };
 
